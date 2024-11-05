fcaa8dbe13f01b28b56c6843f554c781d8954c3b
Kosyo Nikolov <kosyo.nikolov@proton.me>
2024-11-05 20:19:25 +0200

    more fixes

--- jl/localMatrix.jl
+++ jl/localMatrix.jl
@@ -5,15 +5,15 @@ include("coordTransform.jl")
 function calcLocalMassMatrix(nodes::AbstractMatrix{<:Number}, val::Function, xyw::AbstractMatrix{<:Number})
     j = calcRefTriangleTransformJacobian(nodes[:,1], nodes[:,2])
     detJ = det(j)
-    first = val(xyw[1,1], xyw[1,2]) .* xyw[1,3]
-    result = first * first'
-    m = size(xyw)[2]
+    first = val(xyw[1,1], xyw[1,2])
+    result = (first * first') .* xyw[1,3]
+    m = size(xyw)[1]
     for i = 2:m
-        curr = val(xyw[i,1], xyw[i,2]) .* xyw[i,3]
-        result .+= curr * curr'
+        curr = val(xyw[i,1], xyw[i,2]) 
+        result .+= (curr * curr') .* xyw[i,3]
     end
 
-    result .*= detJ
+    result .*= abs(detJ)
     return result
 end
 
@@ -22,10 +22,10 @@ function calcLocalLoadVector(nodes::AbstractMatrix{<:Number}, val::Function, f::
     detJ = det(calcRefTriangleTransformJacobian(nodes[:,1], nodes[:,2]))
 
     result = val(xyw[1,1:2]) .* (xyw[1,3] * f(m * xyw[1,1:2] .+ b))
-    m = size(xyw)[2]
+    m = size(xyw)[1]
     for i = 2:m
         result .+= val(xyw[i,1:2]) .* (xyw[i,3] * f(m * xyw[i,1:2] .+ b))
     end
-    result .*= detJ
+    result .*= abs(detJ)
     return result 
 end
